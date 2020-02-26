405192eceb30a30ded70edbc4e26a3f335e9394e
Nico Salvador <nico.salvador@bax-shop.nl>
2020-02-26 21:45:28 +0800

    Remove unused DB request

--- server/src/reportRenderer.ts
+++ server/src/reportRenderer.ts
@@ -6,10 +6,6 @@ import { MongoConnection } from "./MongoConnection"
 export const displayReport: (
 	req: Koa.ParameterizedContext
 ) => void = async ctx => {
-	// TODO: add separate route for this and fetch asyncronously
-	const db = new MongoConnection()
-	const latestPing = await db.pull()
-
 	ctx.body = pug.renderFile(resolve(__dirname, "page/index.pug"))
 }
 
