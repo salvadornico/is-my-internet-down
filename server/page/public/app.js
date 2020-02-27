5a7ccb977494f56b13e7247b8cdd302192db27a3
Nico Salvador <nico.salvador@bax-shop.nl>
2020-02-27 14:58:32 +0800

    Report layout & behavior adjustments

--- server/page/public/app.js
+++ server/page/public/app.js
@@ -6,18 +6,29 @@ const doubtLevels = [
 ]
 
 document.addEventListener("DOMContentLoaded", () => {
-	const reportLine = document.querySelector("#report")
-
 	fetch("/latest-ping")
 		.then(res => res.json())
 		.then(({ name, time }) => {
+			const report = document.querySelector(".report")
+			const answerLine = document.querySelector(".answer")
+
 			const timeObject = moment(time)
-			// const secondsSincePing = 5 // ?
-			// const answer = doubtLevels.filter(level => (level.minutesAllowance * 60) > secondsSincePing) // Then choose lowest remaining
+			const secondsSincePing = moment().diff(timeObject, "seconds");
+
+			const answer = doubtLevels
+				.filter(level => (level.minutesAllowance * 60) > secondsSincePing)
+				.reduce((res, obj) => (obj.minutesAllowance < res.minutesAllowance) ? obj : res)
+			;
 			// Change CSS variable for background color
 
-			reportLine.textContent = `Last ping received from ${name} ${timeObject.fromNow()} (${timeObject.format(
-				"D MMM YYYY [at] H:mm:ss"
-			)} Philippine Standard Time)`
+			answerLine.textContent = answer.message
+			report.innerHTML = `
+				<p>Last ping received from ${name} ${timeObject.fromNow()}</p>
+				<p>${timeObject.format("D MMM YYYY [at] HH:mm:ss")} Philippine Standard Time</p>
+			`
+
+			document.querySelector(".loading-message").classList.add("hidden")
+			answerLine.classList.remove("hidden")
+			report.classList.remove("hidden")
 		})
 })
