496e958603a4376c350b228682224be383862868
Nico Salvador <salvador.nico@gmail.com>
2020-02-27 10:09:08 +0800

    Add notes for color changing

--- server/page/public/app.js
+++ server/page/public/app.js
@@ -1,9 +1,9 @@
-//- const doubtLevels = [
-//-     { minutesAllowance: 0, message: 'No' },
-//-     { minutesAllowance: 0, message: 'Maybe' },
-//-     { minutesAllowance: 0, message: 'Probably' },
-//-     { minutesAllowance: 0, message: 'Almost definitely' },
-//- ]
+const doubtLevels = [
+	{ minutesAllowance: 5, message: "No", color: "" },
+	{ minutesAllowance: 10, message: "Maybe", color: "" },
+	{ minutesAllowance: 20, message: "Probably", color: "" },
+	{ minutesAllowance: 30, message: "Almost definitely", color: "" }
+]
 
 document.addEventListener("DOMContentLoaded", () => {
 	const reportLine = document.querySelector("#report")
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
 		.then(res => res.json())
 		.then(({ name, time }) => {
 			const timeObject = moment(time)
-			//- const minutesSincePing = 5 // ?
-			//- const answer = doubtLevels.filter(level => level.minutesAllowance > minutesSincePing) // Then choose lowest remaining
+			// const secondsSincePing = 5 // ?
+			// const answer = doubtLevels.filter(level => (level.minutesAllowance * 60) > secondsSincePing) // Then choose lowest remaining
+			// Change CSS variable for background color
 
 			reportLine.textContent = `Last ping received from ${name} ${timeObject.fromNow()} (${timeObject.format(
 				"D MMM YYYY [at] H:mm:ss"
