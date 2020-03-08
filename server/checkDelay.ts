081e7f2e4bc05906986f4076db501df53878e2b8
Nico Salvador <salvador.nico@gmail.com>
2020-03-08 22:21:22 +0800

    Add node-fetch

--- server/checkDelay.ts
+++ server/checkDelay.ts
@@ -1,4 +1,6 @@
 import { config } from "dotenv"
+import fetch from "node-fetch"
+// import moment from "moment"
 import { resolve } from "path"
 import { MongoConnection } from "./db/MongoConnection"
 
@@ -11,9 +13,9 @@ const checkDelay = async () => {
 	config({ path: resolve(__dirname, "../.env") })
 
 	const db = new MongoConnection()
-	const latestNotif = await db.getLastNotification()
-	console.log(latestNotif)
-	process.exit(0)
+	// const latestNotif = await db.getLastNotification()
+	// console.log(latestNotif)
+	// process.exit(0)
 	// const { time } = await db.getLastCheckIn()
 
 	// const timeObject = moment(time)
@@ -23,20 +25,20 @@ const checkDelay = async () => {
 	// 	process.exit(0)
 	// }
 
-	// fetch(process.env.IFTTT_URL)
-	// 	.then(res => res.text())
-	// 	.then(data => {
-	// 		console.log(data)
+	fetch(process.env.IFTTT_URL)
+		.then(res => res.text())
+		.then(data => {
+			console.log(data)
 
-	// 		try {
-	// 			console.log("trying")
-	// 			db.recordNotification({ time: new Date() })
-	// 			process.exit(0)
-	// 		} catch (error) {
-	// 			handleError(error)
-	// 		}
-	// 	})
-	// 	.catch(err => handleError(err))
+			try {
+				console.log("trying")
+				db.recordNotification({ time: new Date() })
+				process.exit(0)
+			} catch (error) {
+				handleError(error)
+			}
+		})
+		.catch(err => handleError(err))
 }
 
 checkDelay()
