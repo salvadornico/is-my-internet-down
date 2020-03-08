f1e837ede88faae86245637966eef34c324fd8f2
Nico Salvador <salvador.nico@gmail.com>
2020-03-08 21:50:34 +0800

    Move IFTTT URL to env

--- server/checkDelay.ts
+++ server/checkDelay.ts
@@ -5,28 +5,28 @@ import { resolve } from "path"
 import { MongoConnection } from "./MongoConnection"
 
 const checkDelay = async () => {
-    config({ path: resolve(__dirname, "../.env") })
+	config({ path: resolve(__dirname, "../.env") })
 
-    const db = new MongoConnection()
-    const { time } = await db.pull()
+	const db = new MongoConnection()
+	const { time } = await db.pull()
 
-    const timeObject = moment(time)
-    const minutesSincePing = moment().diff(timeObject, "minutes")
+	const timeObject = moment(time)
+	const minutesSincePing = moment().diff(timeObject, "minutes")
 
-    if (minutesSincePing < 30) {
-        process.exit(0)
-    }
+	if (minutesSincePing < 30) {
+		process.exit(0)
+	}
 
-    fetch("https://maker.ifttt.com/trigger/check_internet/with/key/pdO7pieMLX-gEqse6DdrKnV6HypeSZyzORO5dt6HmCb")
-        .then(res => res.text())
-        .then(data => {
-            console.log(data)
-            process.exit(0)
-        })
-        .catch(err => {
-            console.error(err)
-            process.exit(1)
-        })
+	fetch(process.env.IFTTT_URL)
+		.then(res => res.text())
+		.then(data => {
+			console.log(data)
+			process.exit(0)
+		})
+		.catch(err => {
+			console.error(err)
+			process.exit(1)
+		})
 }
 
 checkDelay()
